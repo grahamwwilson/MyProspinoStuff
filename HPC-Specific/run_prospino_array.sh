7f5153bc616acffa38e30d79099c8a943c55d6aa
grahamwwilson <gwwilson@ku.edu>
2022-06-11 16:41:15 -0500

    Update batch job

--- HPC-Specific/run_prospino_array.sh
+++ HPC-Specific/run_prospino_array.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Run an array of 51 jobs for one particular input SLHA file. 
+# Each job is one of the 51 production channels (21 for sleptons + 30 for electroweakinos)
+#
+# Please change E-mail to your own E-mail or turn that feature off!
+#
 #SBATCH --job-name=prosp-array-job    # Job name
 #SBATCH --partition=sixhour           # Partition Name (Required)
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -6,7 +12,7 @@
 #SBATCH --nodes=1 --ntasks=1          # Run on a single CPU
 #SBATCH --mem=1g                      # Job memory request
 #SBATCH --time=0-00:50:00             # Time limit days-hrs:min:sec
-#SBATCH --array=1-51                  # Job array (51 in length)  1-21=sleptons, 22-51=neutralinos
+#SBATCH --array=1-51                  # Job array (51 in length)  1-21=sleptons, 22-51=electroweakinos
 #SBATCH --output=prosp-array_%A_%a.log  # Standard output and error log
 #SBATCH --error=prosp-array_%A_%a.err   # Standard output and error log
 
@@ -18,11 +24,19 @@ echo $HOME
 echo $WORK
 echo "SLURM_ARRAY_TASK_ID: "${SLURM_ARRAY_TASK_ID}
 
+# Main directory for the git based code etc.
+# On HPC this should be where you cloned the git repository MyProspinoStuff 
+# You may want to also use your $WORK directory (/panfs/pfs.local/work/wilson/$USER
+GITDIR=$WORK/MyProspinoStuff
+
 # Directory where the executable resides
-EXEDIR=/home/gwwilson/Prospino
+EXEDIR=/panfs/pfs.local/work/wilson/gwwilson/Prospino
+
+# Here we use ${GITDIR}/SLHAFiles/test.slha as the input SLHA file
+SLHA=test
 
 # Directory to run batch job from
-BRDIR=$WORK/ProspinoOut/TestSLHA/Job-${SLURM_ARRAY_TASK_ID}
+BRDIR=$WORK/ProspinoOut/${SLHA}-Job-${SLURM_ARRAY_TASK_ID}
 
 mkdir ${BRDIR}
 cd ${BRDIR}
@@ -33,11 +47,11 @@ pwd
 SLHAFILE=prospino.in.les_houches
 PDSFILE=ctq66.00.pds
 TBLFILE=cteq6l1.tbl
-ARGSFILE=/home/gwwilson/Prepare_for_Launch/sleptons-electroweakinos_commands.txt
+ARGSFILE=${GITDIR}/HPC-Specific/sleptons-electroweakinos_commands.txt
 
 if [ ! -e ${SLHAFILE} ]
 then
-   ln -s ${EXEDIR}/${SLHAFILE} ${SLHAFILE}
+   ln -s ${GITDIR}/SLHAFiles/${SLHA}.slha ${SLHAFILE}
 fi
 
 if [ ! -e ${PDSFILE} ]
@@ -50,12 +64,7 @@ then
    ln -s ${EXEDIR}/${TBLFILE} ${TBLFILE}
 fi
 
-#ln -s ${EXEDIR}/prospino.in.les_houches prospino.in.les_houches
-#ln -s ${EXEDIR}/ctq66.00.pds ctq66.00.pds
-#ln -s ${EXEDIR}/cteq6l1.tbl cteq6l1.tbl
-
 echo "SLURM_ARRAY_TASK_ID: "${SLURM_ARRAY_TASK_ID}
-#LINE=$(sed -n "$SLURM_ARRAY_TASK_ID"p sleptons_commands.txt)
 LINE=$(sed -n "$SLURM_ARRAY_TASK_ID"p ${ARGSFILE})
 echo "Found Prospino arguments : "
 echo $LINE
