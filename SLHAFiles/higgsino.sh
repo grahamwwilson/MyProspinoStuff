d2fa4d8ab93f36f1d88af6fb4598a2081a25e63a
grahamwwilson <gwwilson@ku.edu>
2022-07-04 17:28:11 -0500

    script to generate specified N2/C1/N1 SLHA higgsino file

---
+++ SLHAFiles/higgsino.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+MN2=${1:-250.0}
+MC1=${2:-235.0}
+MN1=${3:-220.0}
+MYFILE=Higgsinos_${MN2}-${MC1}-${MN1}.slha
+
+NMN2=${MN2}E+00
+NMC1=${MC1}E+00
+NMN1=${MN1}E+00
+echo 'New masses = '${NMN2},${NMC1},${NMN1}
+
+cp higgsino_slha_C1C1.in ${MYFILE}
+
+sed -i -e "s/{MN2}/${NMN2}/g" -e "s/{MC1}/${NMC1}/g" -e "s/{MN1}/${NMN1}/g" ${MYFILE}
+
+exit
